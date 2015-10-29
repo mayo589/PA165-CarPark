2f27ee4aa21550fc2324317f8362464a10aaa4cd
Jan Hellar <janhellar@gmail.com>
2015-10-29 12:43:34 +0100

    Implementation of DAO

--- CarPark/CarParkApp/src/main/java/com/fi/muni/carparkapp/dao/OfficeDao.java
+++ CarPark/CarParkApp/src/main/java/com/fi/muni/carparkapp/dao/OfficeDao.java
@@ -1,9 +1,16 @@
 package com.fi.muni.carparkapp.dao;
 
+import com.fi.muni.carparkapp.entity.Office;
+import java.util.List;
+
 /**
  *
  * @author Jan Hellar (436358)
  */
 public interface OfficeDao {
-    
+    public Office findById(Long id);
+    public List<Office> findAll();
+    public Office findByName(String name);
+    public void create(Office o);
+    public void delete(Office o);
 }
--- CarPark/CarParkApp/src/main/java/com/fi/muni/carparkapp/dao/OfficeDaoImpl.java
+++ CarPark/CarParkApp/src/main/java/com/fi/muni/carparkapp/dao/OfficeDaoImpl.java
@@ -1,9 +1,51 @@
 package com.fi.muni.carparkapp.dao;
 
+import com.fi.muni.carparkapp.entity.Office;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+
 /**
  *
  * @author Jan Hellar (436358)
  */
-public class OfficeDaoImpl {
+@Repository
+public class OfficeDaoImpl implements OfficeDao {
+    
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public Office findById(Long id) {
+        return em.find(Office.class, id);
+    }
+
+    @Override
+    public List<Office> findAll() {
+        return em.createQuery("select o from Office o", Office.class)
+                .getResultList();
+    }
+
+    @Override
+    public Office findByName(String name) {
+        try {
+            return em.createQuery("select c from Office o where name = :name",
+                    Office.class).setParameter(":name", name).getSingleResult();
+        } catch (NoResultException nre) {
+            return null;
+        }
+    }
+
+    @Override
+    public void create(Office o) {
+        em.persist(o);
+    }
+
+    @Override
+    public void delete(Office o) {
+        em.remove(o);
+    }
     
 }
