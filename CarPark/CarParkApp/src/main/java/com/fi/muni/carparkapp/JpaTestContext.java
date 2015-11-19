12d65effa86864be8ed31ab97f4edc21ea4a7c78
Marek Mihalech <marekmihalech@gmail.com>
2015-11-19 10:28:34 +0100

    Added CarParkAPI and CarParkService modules

--- CarPark/CarParkApp/src/main/java/com/fi/muni/carparkapp/JpaTestContext.java
+++ CarPark/CarParkApp/src/main/java/com/fi/muni/carparkapp/JpaTestContext.java
@@ -16,12 +16,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.instrument.classloading.InstrumentationLoadTimeWeaver;
+import org.springframework.instrument.classloading.LoadTimeWeaver;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 /**
  *
@@ -34,22 +37,32 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class JpaTestContext {
     
     @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-        LocalContainerEntityManagerFactoryBean lcemfb = new LocalContainerEntityManagerFactoryBean();
-        lcemfb.setDataSource(ds());
-        lcemfb.setPersistenceProviderClass(HibernatePersistenceProvider.class);
-        return lcemfb;
-    }
-    
-    @Bean
-    public JpaTransactionManager transactionManager() {
-        return new JpaTransactionManager(entityManagerFactory().getObject());
+    public JpaTransactionManager transactionManager(){
+        return  new JpaTransactionManager(entityManagerFactory().getObject());
     }
 
     @Bean
-    public DataSource ds() {
-        EmbeddedDatabaseBuilder edbBuilder = new EmbeddedDatabaseBuilder();
-        EmbeddedDatabase db = edbBuilder.setType(EmbeddedDatabaseType.DERBY).build();
+    public LocalContainerEntityManagerFactoryBean  entityManagerFactory(){
+        LocalContainerEntityManagerFactoryBean jpaFactoryBean = new LocalContainerEntityManagerFactoryBean ();
+        jpaFactoryBean.setDataSource(db());
+        jpaFactoryBean.setLoadTimeWeaver(instrumentationLoadTimeWeaver());
+        jpaFactoryBean.setPersistenceProviderClass(HibernatePersistenceProvider.class);
+        return jpaFactoryBean;
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean localValidatorFactoryBean(){
+        return new LocalValidatorFactoryBean();
+    }
+    @Bean
+    public LoadTimeWeaver instrumentationLoadTimeWeaver() {
+        return new InstrumentationLoadTimeWeaver();
+    }
+
+    @Bean
+    public DataSource db(){
+        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+        EmbeddedDatabase db = builder.setType(EmbeddedDatabaseType.DERBY).build();
         return db;
     }
     
