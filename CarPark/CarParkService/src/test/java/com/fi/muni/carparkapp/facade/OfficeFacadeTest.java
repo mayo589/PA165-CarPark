f79e77c5ea02229d3bd8c15d5721f1c53b9a6af9
Jan <janhellar@gmail.com>
2015-11-27 21:41:44 +0100

    Added office facade test

--- CarPark/CarParkService/src/test/java/com/fi/muni/carparkapp/facade/OfficeFacadeTest.java
+++ CarPark/CarParkService/src/test/java/com/fi/muni/carparkapp/facade/OfficeFacadeTest.java
@@ -4,8 +4,12 @@ import com.fi.muni.carparkapp.dto.OfficeDTO;
 import com.fi.muni.carparkapp.entity.Office;
 import com.fi.muni.carparkapp.service.OfficeService;
 import com.fi.muni.carparkapp.service.config.ServiceConfiguration;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -44,6 +48,33 @@ public class OfficeFacadeTest extends AbstractTransactionalTestNGSpringContextTe
         
         OfficeDTO of = officeFacade.findOfficeById(1L);
         Assert.assertEquals(of, od);
+    }
+    
+    @Test
+    public void getAllOfficesTest() {
+        Office o = new Office(1L);
+        OfficeDTO od = new OfficeDTO();
+        od.setId(1L);
+        
+        List<Office> lo = new ArrayList<>();
+        lo.add(o);
+        List<OfficeDTO> lod = new ArrayList<>();
+        lod.add(od);
+        
+        when(officeService.getAllOffices()).thenReturn(lo);
+        
+        Collection<OfficeDTO> of = officeFacade.getAllOffices();
+        Assert.assertEquals(of.size(), lod.size());
+    }
+    
+    @Test
+    public void addOfficeTest() {
+        Office o = new Office(1L);
+        OfficeDTO od = new OfficeDTO();
+        od.setId(1L);
+        
+        officeFacade.AddOffice(od);
+        verify(officeService).AddOffice(o);
     }
     
 }
