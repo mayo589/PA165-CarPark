4be9207fb86cd12a4c59d90f06efbe2740ca3357
Jan Starka <Jan Starka@JanStarka-PC>
2015-11-27 22:13:38 +0100

    ReservationFacade javadoc added

--- CarPark/CarParkApi/src/main/java/com/fi/muni/carparkapp/facade/ReservationFacade.java
+++ CarPark/CarParkApi/src/main/java/com/fi/muni/carparkapp/facade/ReservationFacade.java
@@ -19,9 +19,34 @@ public interface ReservationFacade {
      * @return reservation for given id
      */
     ReservationDTO getReservationById(Long reservationId);
+    
+    /**
+     * Gets reservations for given employee
+     * @param employee employee for whom the reservation is given
+     * @return reservation for given employee
+     */
     Collection<ReservationDTO> getAllReservationsForEmployee(EmployeeDTO employee);
+    
+    /**
+     * Gets reservations for given car
+     * @param car car for which the reservation is given
+     * @return reservation for given car
+     */
     Collection<ReservationDTO> getAllReservationsForCar(CarDTO car);
+    
+    /**
+     * Gets reservations for given office
+     * @param office office for which the reservation is given
+     * @return reservation for given office
+     */
     Collection<ReservationDTO> getAllReservationsForOffice(OfficeDTO office);
+    
+    /**
+     * Gets reservations for given date range
+     * @param fromDate beginning date
+     * @param toDate ending date
+     * @return reservations for given date range
+     */
     Collection<ReservationDTO> getReservationsForDateRange(Date fromDate, Date toDate);
     void addReservation(ReservationDTO reservationDTO);
     void cancelReservation(ReservationDTO reservationDTO);
