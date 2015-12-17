95a311ef262058e900158f175b9650493ccd71cf
Jan <janhellar@gmail.com>
2015-12-17 15:45:29 +0100

    OfficeDAO transactional

--- CarPark/CarParkPersistence/src/main/java/com/fi/muni/carparkapp/dao/OfficeDaoImpl.java
+++ CarPark/CarParkPersistence/src/main/java/com/fi/muni/carparkapp/dao/OfficeDaoImpl.java
@@ -14,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
  * @author Jan Hellar (436358)
  */
 @Repository
+@Transactional
 public class OfficeDaoImpl implements OfficeDao {
     
     @PersistenceContext
